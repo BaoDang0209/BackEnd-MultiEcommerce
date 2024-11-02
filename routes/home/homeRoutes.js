367d6a92fbf0c5381b53b7478e38988b7f04c365
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-02 15:18:48 +0700

    update homereducer

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -2,6 +2,7 @@ const homeControllers = require('../../controllers/home/homeControllers')
 const router = require('express').Router()
 
 router.get('/home/get-categorys', homeControllers.get_categorys);
+router.get('/home/get-products',homeControllers.get_products)
   
 
 module.exports = router 

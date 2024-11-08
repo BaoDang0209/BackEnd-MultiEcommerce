619c62b48ad7d993f4b46691510176c36dce87dd
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-08 15:46:54 +0700

    add 3 api product

--- controllers/home/homeControllers.js
+++ controllers/home/homeControllers.js
@@ -71,6 +71,105 @@ class homeControllers{
     }
    // end method 
 
+   price_range_product = async (req, res) => {
+    try {
+        const priceRange = {
+            low: 0,
+            high: 0,
+        }
+        const products = await productModel.find({}).limit(9).sort({
+            createdAt: -1 // 1 for asc -1 is for Desc
+        })
+        const latest_product = this.formateProduct(products);
+        const getForPrice = await productModel.find({}).sort({
+            'price': 1
+        })
+        if (getForPrice.length > 0) {
+            priceRange.high = getForPrice[getForPrice.length - 1].price
+            priceRange.low = getForPrice[0].price
+        }
+        responseReturn(res, 200, {
+            latest_product,
+            priceRange
+        })
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+
+   }
+
+// end method 
+
+query_products = async (req, res) => {
+    const parPage = 12
+    req.query.parPage = parPage
+
+    try {
+        const products = await productModel.find({}).sort({
+            createdAt: -1
+        })
+        const totalProduct = new queryProducts(products, req.query).categoryQuery().ratingQuery().searchQuery().priceQuery().sortByPrice().countProducts();
+
+        const result = new queryProducts(products, req.query).categoryQuery().ratingQuery().priceQuery().searchQuery().sortByPrice().skip().limit().getProducts();
+        
+        responseReturn(res, 200, {
+            products: result,
+            totalProduct,
+            parPage
+        })
+
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+ 
+}
+// end method 
+
+product_details = async (req, res) => {
+    const { slug } = req.params
+    try {
+        const product = await productModel.findOne({slug})
+        
+        const relatedProducts = await productModel.find({
+            $and: [{
+                _id: {
+                    $ne: product.id
+                }
+            },
+            {
+                category: {
+                    $eq: product.category 
+                }
+            }
+           ]
+        }).limit(12)
+        const moreProducts = await productModel.find({
+            $and: [{
+                _id: {
+                    $ne: product.id
+                }
+            },
+            {
+                sellerId: {
+                    $eq: product.sellerId
+                }
+            }
+           ]
+        }).limit(3)
+        responseReturn(res, 200, {
+            product,
+            relatedProducts,
+            moreProducts
+        })
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+// end method 
+
 
 }
 

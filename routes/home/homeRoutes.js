26f44f68c06962cd62284781029be4acf9bd5905
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-08 15:45:44 +0700

    add 3 routes product

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -3,6 +3,10 @@ const router = require('express').Router()
 
 router.get('/home/get-categorys', homeControllers.get_categorys);
 router.get('/home/get-products',homeControllers.get_products)
+
+router.get('/home/price-range-latest-product',homeControllers.price_range_product)
+router.get('/home/query-products',homeControllers.query_products)
+router.get('/home/product-details/:slug',homeControllers.product_details)
   
 
 module.exports = router 

eba11fb5f8754510843bc28f80ca8694747c98fc
Trườngg Đỗ <97582059+bibibibibibibibibi@users.noreply.github.com>
2024-11-08 22:27:28 +0700

    Revert "customer"
    This reverts commit c395650e5cd15b8bf053e4fb9c3df3a45fa87ec7.

--- models/customerModel.js
+++ models/customerModel.js
@@ -14,18 +14,6 @@ const customerSchema = new Schema({
         required : true,
         select: false
     },
-    address:{
-        type: String,
-        required : true,
-    },
-    phoneNumber:{
-        type: String,
-        required : true,
-    },
-    image:{
-        type: String,
-        required : true,
-    },
     method: {
         type: String,
         required : true

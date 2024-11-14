268ba5ba78fc59f098cfec05645a619066c6c38a
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-15 02:23:00 +0700

    Show/get order data

--- routes/order/orderRoutes.js
+++ routes/order/orderRoutes.js
@@ -1,9 +1,15 @@
 const orderController = require('../../controllers/order/orderController')
 const router = require('express').Router()
 
+//Customers
 router.post('/home/order/place-order',orderController.place_order)
 router.get('/home/customer/get-dashboard-data/:userId',orderController.get_dashboard_data)
 router.get('/home/coustomer/get-orders/:customerId/:status',orderController.get_orders)
 router.get('/home/coustomer/get-order-details/:orderId',orderController.get_order_details)
+
+//Sellers
+router.get('/seller/orders/:sellerId',orderController.get_seller_orders)
+router.get('/seller/order/:orderId',orderController.get_seller_order)
+router.put('/seller/order-status/update/:orderId',orderController.seller_order_status_update)
 
 module.exports = router  

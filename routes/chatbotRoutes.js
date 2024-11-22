2b8012a7dc61d7a0761980cbd7ec116fb723910c
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-22 21:37:37 +0700

    Revert "Done chatbot part1"
    This reverts commit e2f8d762c9227f874f3d5e1d68ab4179e5651889.

--- routes/chatbotRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { handleChatRequest } = require('../controllers/chatgpt/ChatbotController')
-const router = express.Router();
-
-router.post('/chatbot', handleChatRequest);
-
-module.exports = router;
